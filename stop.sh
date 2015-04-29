54f4b708995fa111ecd8592c88709b5b8ef9c75d
Caleb Adams <cmadams7@liberty.edu>
2015-04-29 00:47:06 -0400

    Changing stop.sh

--- stop.sh
+++ stop.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-killall python
-killall ping
-killall bash
+kill -9 $(ps aux | grep scapy-sniffer.py | grep -v grep | awk '{ print $2 }')
+kill -9 $(ps aux | grep ping | grep -v grep | awk '{ print $2 }')
